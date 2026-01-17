ed84f8c983173aac358646a6dfafb5c6c1b9bc80
guziczak <guziczak@proton.me>
2026-01-17 09:34:14 +0100

    update

--- check_wal.ps1
+++
@@ -1,4 +0,0 @@
-$base = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Network"
-Get-ChildItem $base -ErrorAction SilentlyContinue | ForEach-Object {
-    Write-Host "$($_.Name): $($_.Length) bytes"
-}
